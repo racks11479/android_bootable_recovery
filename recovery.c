b33f91b306f74e1a117a56310bf9c7152a82a94d
racks11479 <racks11479@gmail.com>
2012-02-21 17:24:47 -0500

    Touch Recovery for ICS

--- recovery.c
+++ recovery.c
@@ -44,6 +44,9 @@
 #include "extendedcommands.h"
 #include "flashutils/flashutils.h"
 
+#define ABS_MT_POSITION_X 0x35  /* Center X ellipse position */
+#define ABS_MT_POSITION_Y 0x36  /* Center Y ellipse position */
+
 static const struct option OPTIONS[] = {
   { "send_intent", required_argument, NULL, 's' },
   { "update_package", required_argument, NULL, 'u' },
@@ -446,7 +449,9 @@ get_menu_selection(char** headers, char** items, int menu_only,
     int wrap_count = 0;
 
     while (chosen_item < 0 && chosen_item != GO_BACK) {
-        int key = ui_wait_key();
+		struct keyStruct *key;
+		key = ui_wait_key();
+
         int visible = ui_text_visible();
 
         if (key == -1) {   // ui_wait_key() timed out
@@ -459,7 +464,11 @@ get_menu_selection(char** headers, char** items, int menu_only,
             }
         }
 
-        int action = device_handle_key(key, visible);
+		int action;
+		if(key->code == ABS_MT_POSITION_X)
+	        action = device_handle_mouse(key, visible);
+		else
+	        action = device_handle_key(key->code, visible);
 
         int old_selected = selected;
 
